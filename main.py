f22a9f491a5df91d0439f577e810c30865e545a1
Temur Tsomaia <tim.tsomaya663@gmail.com>
2024-11-22 20:08:22 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def add(cls,a,b):
+        return a+b
+
+    @classmethod
+    def multiply(cls,a,b):
+        return a*b
